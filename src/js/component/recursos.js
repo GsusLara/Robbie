94d929339edf1855d928b3c7f15177385ce00b88
GsusLara <jelara99@gmail.com>
2021-06-17 15:48:21 -0600

    trabajo en vista

--- src/js/component/recursos.js
+++ src/js/component/recursos.js
@@ -10,79 +10,99 @@ export const Recursos = () => {
 
 	return (
 		<div className="container-fluid m-0 p-0">
-			<div className="margen"></div>
+			<div className="margen" /> {/*margen para el navbar */}
+			{/*contenedor de la primera parte*/}
 			<div className="container-fluid m-0 p-0 mb-5 recurso1">
-				<div className="row">
-					<div className="col-12 text-center mt-lg-3 pl-lg-5 text-lg-left ">
-						<span className="font-weight-bold" style={{ fontSize: "1.5rem" }}>
-							¿Como{" "}
-						</span>
+				<div className="row justify-content-center">
+					{/*contenedor del titulo*/}
+					<div className="col-12 text-center mt-lg-3 pl-lg-5 text-lg-left">
+						<span className="titulo2">¿Como </span>
 						<span className="font-weight-bold">iniciar con </span>
 						<span className="titulo1">Robbie++ </span>
 						<span className="titulo2">?</span>
 					</div>
-
+					{/*primera caja */}
 					<div
 						id="espacio1"
-						className="col-12 col-lg-4 mt-3 mt-lg-5 d-flex flex-column justify-content-center espacio ">
-						<div className="mr-5 p-5 espacioInterno">
-							<strong>Conoce la propuesta LIE++ preescolar</strong>
+						className="col-12 col-md-7 col-lg-4 mt-2 mt-lg-5 d-flex flex-column justify-content-center  espacio">
+						<div className="col-8 mx-auto d-flex flex-column justify-content-center espacioInterno">
+							<strong>
+								Conoce la propuesta LIE++ <br /> preescolar
+							</strong>
+							<button type="button" className="btn btn-light mx-auto mt-5 descarga">
+								Descargar
+							</button>
 						</div>
 					</div>
+					{/*segunda caja */}
 					<div
 						id="espacio2"
-						className="col-12 col-lg-4 mt-2  mt-lg-5 d-flex flex-column justify-content-center text-wrap espacio">
-						<div className="mr-5 p-5 espacioInterno">
-							<div className="p-lg-4 mr-lg-2">
-								<strong>Aprende sobre su Arquitectura y componentes</strong>
-							</div>
+						className="col-12 col-md-7 col-lg-4 mt-2  mt-lg-5 d-flex flex-column justify-content-center  espacio">
+						<div className="col-8 mx-auto d-flex flex-column justify-content-center espacioInterno">
+							<strong>
+								Aprende sobre su Arquitectura <br /> y componentes
+							</strong>
 						</div>
 					</div>
+					{/*tercera caja */}
 					<div
 						id="espacio3"
-						className="col-12 mb-lg-5 mb-lg-0 col-lg-4 mt-lg-5 d-flex flex-column justify-content-center espacio">
-						<div className="mx-5 px-4 espacioInterno">
+						className="col-12 col-md-7 col-lg-4 mt-2  mt-lg-5 d-flex flex-column justify-content-center  espacio">
+						<div className="col-8 mx-auto d-flex flex-column justify-content-center espacioInterno">
 							<strong>Sincroniza Robbie++ y tititbots</strong>
 						</div>
 					</div>
 				</div>
 			</div>
-
+			{/*contenedor de la segunda parte*/}
 			<div className="container mt-lg-5 mb-5 recurso2">
 				<div className="row">
-					<div className="col-12 col-lg-6">
-						<div className="col-12 text-left mt-lg-5" style={{ height: "30vh" }}>
-							<h4>
-								<strong>Empieza a programar</strong>
-							</h4>
-							<img className="decoracion" src={linea} />
+					{/*lado izquierdo*/}
+					<div className="col-12 col-md-6">
+						{/*contenedor del titulo*/}
+						<div className="col-12 text-left mt-lg-5">
+							<span>¡Vamos a </span>
+							<spam className="titulo2">programar!</spam>
 						</div>
-						<div className="col-11 text-left mt-5 mt-lg-0">
-							<a className="p-3" href={linkMover} target="blank">
-								Avanzar
-							</a>
+						{/*decorador*/}
+						<div className="col-6  col-lg-6">
+							<img className="img-fluid" src={linea} />
 						</div>
-						<div className="col-11 text-left mt-2 ">
-							<a className="p-3" href={linkGiro} target="blank">
-								¿Como girar?
-							</a>
-						</div>
-						<div className="col-11 text-left mt-2">
-							<a className="p-3" href={linkPinzas} target="blank">
-								Utilizar pinzas
-							</a>
-						</div>
-						<div className="col-11 text-left mt-2">
-							<a className="p-3" href={linkLuz} target="blank">
-								Enciende su luz
-							</a>
+						<div className="col-6  col-lg-6 text-right text-md-center mt-lg-2">
+							<ul className="list-unstyled text-nowrap">
+								<li className="p-2">
+									<span>Como </span>
+									<a className="videoLink" href={linkMover} target="blank">
+										avanzar
+									</a>
+								</li>
+								<li className="p-2">
+									<span>Aprende a </span>
+									<a className="videoLink" href={linkGiro} target="blank">
+										girar
+									</a>
+								</li>
+								<li className="p-2">
+									<span>Utiliza las </span>
+									<a className="videoLink" href={linkPinzas} target="blank">
+										pinzas
+									</a>
+								</li>
+								<li className="p-2">
+									<span>Enciende su </span>
+									<a className="videoLink" href={linkLuz} target="blank">
+										luz
+									</a>
+								</li>
+							</ul>
 						</div>
 					</div>
-					<div className="col-12 mt-5 col-lg-6 mt-lg-0 d-flex flex-column justify-content-center">
-						<div className="col-11 text-left col-lg-12 ml-lg-5 p-lg-5 text-lg-center">
-							<span>Lo lograste!</span>
+					{/*lado derecho*/}
+					<div className="col-12 col-md-6 mt-2 mt-lg-0 d-flex flex-column justify-content-center">
+						<div className="col-8 col-md-11 text-left ml-md-5 p-md-5 text-md-center d-flex flex-column justify-content-center">
+							<span className="titulo2">¡Lo lograste!</span>
 							<br />
-							<span>Crea desafios y disfrutemos de aprender jugando</span>
+							<span className="textResponsive">Crea desafios y disfruta de aprender jugando</span>
 						</div>
 					</div>
 				</div>
